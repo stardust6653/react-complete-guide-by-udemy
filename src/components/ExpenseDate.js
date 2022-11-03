542815a75dde2d24918a783c73cd81ecf7fd692e
stardust6653 <stardust6653@gmail.com>
2022-11-04 00:43:22 +0900

    Feat: 물품 컴포넌트 생성
    - ExpenseItem 및 ExpenseDate 컴포넌트 생성
    - CSS 적용

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+function ExpenseDate({ date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
